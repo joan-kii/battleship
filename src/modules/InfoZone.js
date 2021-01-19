97db28fbf8332fcbbe727ef9a784db5c63d956f0
joan-kii <joan.sb81@gmail.com>
2021-01-19 05:26:04 +0100

    añade render ships. pendiente de estilos ships

--- src/modules/InfoZone.js
+++ src/modules/InfoZone.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InfoZone = () => {
   return (
-    <div>
+    <div className='grid-infoZone'>
             
     </div>
   )
--- src/modules/PlayerZone.js
+++ src/modules/PlayerZone.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import gameboardFactory from '../factories/gameboardFactory';
+
+const shipDivs = (ship) => {
+  for(let i = 0; i < ship.shipLength; i++) {
+    <div key={i}></div>
+  }
+};
 
 const PlayerZone = () => {
+
+  const playerFleet = gameboardFactory().shipsArray;
+  const renderPlayerFleet = playerFleet.map((ship, index) => {
+    return <div className={`ship ${ship.shipName}-container`} key={index} draggable='true'>
+      {shipDivs(ship)}
+    </div>
+  });
+
   return (
     <div className='grid-playerZone'>
-            
+      { renderPlayerFleet }   
     </div>
   )
 };
